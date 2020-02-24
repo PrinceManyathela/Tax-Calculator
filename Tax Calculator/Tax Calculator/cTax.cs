fbda6bc1f4e541a14f94764a33e82a8e2ba7b184
PrinceManyathela <51157075+PrinceManyathela@users.noreply.github.com>
2020-02-24 10:50:44 +0200

    Add files via upload

---
+++ Tax Calculator/Tax Calculator/cTax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tax_Calculator
+{
+    class cTax
+    {
+        public double item1;
+        public double ncTax_Rate = 0.65;
+
+        public double GetAmount()
+        {
+            return item1;
+        }
+
+        public Double cFindTax(double cAmount)
+        {
+            double FindTax = cAmount - (cAmount * ncTax_Rate);
+            return FindTax;
+        }
+
+    }
+}
